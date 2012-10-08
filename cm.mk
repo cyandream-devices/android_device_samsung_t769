242ca87fc848aee635084430f7ca3f608672c47c
PlayfulGod <playfulgod@gmail.com>
2012-10-08 10:37:55 -0400

    fixed for Jellybean build of CWM
    Change-Id: I05ad59676f1e939d6bd9e18e0e07dc83f0e187b5

--- cm.mk
+++ cm.mk
@@ -11,8 +11,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/T769/device_T769.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := T769 
-PRODUCT_NAME := cm_T769 
+PRODUCT_DEVICE := T769
+PRODUCT_NAME := cm_T769
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := T769 
+PRODUCT_MODEL := T769
 PRODUCT_MANUFACTURER := samsung
